294419b595d1aa4bc9c6ee6e131086e0f652ddae
xushuangqing <thuxushuangqing@gmail.com>
2025-07-16 23:07:22 +0800

    feat: 🎸 update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/blog-official/', // 👈 这里加上你的仓库名
   plugins: [react()],
-  base: '/blog-official/',
 })
